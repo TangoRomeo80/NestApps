c35f72ab44fbb89370e121e7dca4db8202d71285
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-16 23:59:45 +0600

    finalized the project

--- expense-app/src/custom.interceptor.ts
+++ expense-app/src/custom.interceptor.ts
@@ -3,17 +3,15 @@ import { map } from 'rxjs';
 
 export class CustomInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler) {
-    console.log('This is Intercepting the request');
-    console.log({ context });
-
     return handler.handle().pipe(
       map((data) => {
         const response = {
           ...data,
           createdAt: data.created_at,
         };
-        delete response.created_at;
         delete response.updated_at;
+        delete response.created_at;
+
         return response;
       }),
     );
